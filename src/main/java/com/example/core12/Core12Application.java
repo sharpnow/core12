5ae1817afe05d398a026c91a4d19973861924cfe
sharpnow <gzc1998srp@163.com>
2022-09-09 20:01:09 +0800

    first coommit! For practice the spring.

---
+++ src/main/java/com/example/core12/Core12Application.java
@@ -0,0 +1,13 @@
+package com.example.core12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Core12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Core12Application.class, args);
+    }
+
+}
